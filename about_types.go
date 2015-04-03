0489b99ef78b3ad1b43627e961d33165e7c959de
gere <gerevini@gmail.com>
2015-04-03 17:41:50 +0200

    a bunch of stuff

--- about_types.go
+++ about_types.go
@@ -6,8 +6,13 @@ func (cn coolNumber) multiplyByTwo() int {
 	return int(cn) * 2
 }
 
+func (cn coolNumber) multiplyByX(x int) int {
+	return int(cn) * x
+}
+
 func aboutTypes() {
 	i := coolNumber(4)
-	assert(i == coolNumber(__int__))     // values can be converted between compatible types
-	assert(i.multiplyByTwo() == __int__) // you can add methods on any type you define
+	assert(i == coolNumber(4))     // values can be converted between compatible types
+	assert(i.multiplyByTwo() == 8) // you can add methods on any type you define
+	assert(i.multiplyByX(3) == 12)
 }
